a296a370fae448fcb743ccaa494395cad836ed85
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-02-06 00:15:37 +0200

    Refactoring StringCalculatorImp

--- StringCalculator/StringCalculatorImp.cs
+++ StringCalculator/StringCalculatorImp.cs
@@ -10,51 +10,78 @@ public class StringCalculatorImp : StringCalculator
 {
     public int Add(string numbers)
     {
+        var customDelimiter = GetCustomDelimeter(numbers);
+
+        var numbersSplitted = GetNumbersSplitted(numbers, customDelimiter);
+
+        var numbersList = GetNumbersList(numbersSplitted);
+
+        CheckIfNumbersHasNegativeValues(numbersList);
+
+        return GetSum(numbersList);
+    }
+
+    private string GetCustomDelimeter(string numbers)
+    {
+        return numbers.StartsWith("//") ? numbers[2].ToString() : "\n";
+    }
+
+    private List<List<string>> GetNumbersSplitted(string numbers, string customDelimiter)
+    {
         var commaDelimiter = ",";
-        var customDelimiter = "\n";
+        var numbersSplittedWithComma = numbers.Split(commaDelimiter).ToList();
 
-        if (numbers.StartsWith("//"))
-        {
-            customDelimiter = numbers[2].ToString();
-        }
+        var numbersSplittedWithCustomDelimiter = new List<List<string>>();
+        numbersSplittedWithComma.ForEach(num => numbersSplittedWithCustomDelimiter.Add(num.Split(customDelimiter).ToList()));
 
-        var numbersList = numbers.Split(commaDelimiter).ToList();
+        return numbersSplittedWithCustomDelimiter;
+    }
 
-        var numbersListWithCustomDelimiterSupported = new List<List<string>>();
-        numbersList.ForEach(num => numbersListWithCustomDelimiterSupported.Add(num.Split(customDelimiter).ToList()));
+    private List<int> GetNumbersList(List<List<string>> numbersSplitted)
+    {
+        var numbersList = new List<int>();
 
-        var sum = 0;
-
-        var negativeNumbers = new List<int>();
-
-        numbersListWithCustomDelimiterSupported
+        numbersSplitted
             .SelectMany(x => x)
             .ToList()
             .ForEach(num =>
             {
                 Int32.TryParse(num, out var parsedNumber);
-
-                if (parsedNumber < 0)
-                {
-                    negativeNumbers.Add(parsedNumber);
-                    parsedNumber = 0;
-                }
-
-                if(parsedNumber > 1000)
-                {
-                    parsedNumber = 0;
-                }
-
-                sum += parsedNumber;
+                numbersList.Add(parsedNumber);
             });
 
-        if (negativeNumbers.Count > 0)
+        return numbersList;
+    }
+
+    private void CheckIfNumbersHasNegativeValues(List<int> numbers)
+    {
+        var negativeNumbersCount = numbers
+            .Where(num => num < 0)
+            .ToList()
+            .Count;
+
+        if (negativeNumbersCount > 0)
         {
             string nums = "";
-            negativeNumbers.ForEach(n => nums += n.ToString() + ",");
-            throw new Exception($"Negatives {nums} not allowed, ");
+            numbers.ForEach(n => nums += n.ToString() + ",");
+            throw new Exception($"Negatives {nums} not allowed.");
         }
+    }
 
-        return sum;
+    private int GetSum(List<int> numbersList)
+    {
+        return numbersList
+            .Where(num => !IsBigNumber(num) && !IsNegativeNumber(num))
+            .Sum(x => x);
+    }
+
+    private bool IsBigNumber(int num)
+    {
+        return num > 1000;
+    }
+
+    private bool IsNegativeNumber(int num)
+    {
+        return num < 0;
     }
 }
